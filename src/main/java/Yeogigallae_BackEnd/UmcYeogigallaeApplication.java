328ea7fb95b5b57aa0a47902b21507f293d96d85
parkmineum <a01087164428@gmail.com>
2024-12-30 19:17:27 +0900

    Initial Settings

---
+++ src/main/java/Yeogigallae_BackEnd/UmcYeogigallaeApplication.java
@@ -0,0 +1,13 @@
+package Yeogigallae_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcYeogigallaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcYeogigallaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Yeogigallae_BackEnd/UmcYeogigallaeApplicationTests.java
@@ -0,0 +1,13 @@
+package Yeogigallae_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcYeogigallaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
