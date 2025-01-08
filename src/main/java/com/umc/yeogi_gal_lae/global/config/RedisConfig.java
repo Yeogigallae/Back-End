6b8567a8d419ceab6f92376f437c5f69619bcb05
parkmineum <a01087164428@gmail.com>
2025-01-08 18:31:40 +0900

    [feat] Redis Setting

---
+++ src/main/java/com/umc/yeogi_gal_lae/global/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.umc.yeogi_gal_lae.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+}
