11d1d0b6c2462b8fbc1e447c4cf5b4d8b186d2ed
GithubKangMin <158579562+GithubKangMin@users.noreply.github.com>
2025-01-12 00:59:42 +0900

    [feat] 방 생성 기능 구현

---
+++ src/main/java/com/umc/yeogi_gal_lae/domain/room/domain/RoomMemberId.java
@@ -0,0 +1,26 @@
+package com.umc.yeogi_gal_lae.domain.room.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Embeddable
+public class RoomMemberId implements Serializable {
+
+    @Column(name = "room_id") // 매핑 이름 명시
+    private Long roomId;
+
+    @Column(name = "user_id") // 매핑 이름 명시
+    private Long userId;
+
+    // 기본 생성자
+    public RoomMemberId() {}
+
+    public RoomMemberId(Long roomId, Long userId) {
+        this.roomId = roomId;
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/com/umc/yeogi_gal_lae/domain/room/dto/RoomDto.java
@@ -0,0 +1,11 @@
+package com.umc.yeogi_gal_lae.domain.room.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RoomDto {
+    private Long id; // 방 ID
+    private String name; // 방 이름
+}
