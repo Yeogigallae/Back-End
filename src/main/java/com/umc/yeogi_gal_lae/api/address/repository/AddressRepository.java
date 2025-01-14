e5e808effb54680c02632aabfe91908e51c9514a
Gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-01-14 15:59:00 +0900

    [feat] 여행 장소 domain 설정

--- src/main/java/com/umc/yeogi_gal_lae/api/address/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.umc.yeogi_gal_lae.api.address.repository;
-
-import com.umc.yeogi_gal_lae.api.address.domain.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
--- src/main/java/com/umc/yeogi_gal_lae/api/place/domain/Place.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/place/domain/Place.java
@@ -1,37 +1,37 @@
-package com.umc.yeogi_gal_lae.api.address.domain;
+package com.umc.yeogi_gal_lae.api.place.domain;
 
-import com.umc.yeogi_gal_lae.global.common.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "address")
 @Builder
+@Table(name = "trip_place")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Address extends BaseEntity {
+public class Place {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String address;
 
+    @Column(nullable = false)
+    private String placeName;
+
+    @Column(nullable = false)
     private Double latitude;
 
+    @Column(nullable = false)
     private Double longitude;
-
-    @Builder
-    public Address(String address, Double latitude, Double longitude) {
-        this.address = address;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
 }
