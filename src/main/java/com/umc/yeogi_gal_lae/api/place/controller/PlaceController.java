ed01687bd4e64eed1c6c5172ed3e228596e782af
Gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-01-15 13:09:51 +0900

    [feat] 모든 여행 장소 조회 response, 추가 및 controller 수정

--- src/main/java/com/umc/yeogi_gal_lae/api/place/controller/PlaceController.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/place/controller/PlaceController.java
@@ -3,6 +3,7 @@ package com.umc.yeogi_gal_lae.api.place.controller;
 import com.umc.yeogi_gal_lae.api.place.converter.PlaceConverter;
 import com.umc.yeogi_gal_lae.api.place.domain.Place;
 import com.umc.yeogi_gal_lae.api.place.dto.request.PlaceRequest;
+import com.umc.yeogi_gal_lae.api.place.dto.response.AllPlaceResponse;
 import com.umc.yeogi_gal_lae.api.place.dto.response.PlaceResponse;
 import com.umc.yeogi_gal_lae.api.place.service.PlaceService;
 import com.umc.yeogi_gal_lae.global.response.Response;
@@ -10,9 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,21 +35,17 @@ public class PlaceController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TripPlaceResponse> getTripPlaceById(@PathVariable Long id) {
-        TripPlaceResponse response = tripPlaceService.getTripPlaceById(id);
+    public Response<PlaceResponse> getPlaceById(@PathVariable Long id) {
+        PlaceResponse response = placeService.getPlaceById(id);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping
-    public ResponseEntity<List<TripPlaceResponse>> getAllTripPlaces() {
-        List<TripPlaceResponse> responses = tripPlaceService.getAllTripPlaces();
-        return ResponseEntity.ok(responses);
-    }
+    public Response<AllPlaceResponse> getAllPlaces() {
 
-    // ResourceNotFoundException에 대한 예외 처리기
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        List<Place> places = placeService
+        List<PlaceResponse> responses = placeService.getAllPlaces();
+        return ResponseEntity.ok(responses);
     }
 
     // 추가적인 예외 처리기를 여기에 추가할 수 있습니다.
