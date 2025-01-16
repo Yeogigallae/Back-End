1e9086f306debd1e18caa3c066fb511aa8da4742
parkmineum <a01087164428@gmail.com>
2025-01-16 23:07:59 +0900

    [refactor] Repository 로직 수정/리팩토링

--- src/main/java/com/umc/yeogi_gal_lae/api/user/repository/UserRepository.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/user/repository/UserRepository.java
@@ -1,9 +1,18 @@
 package com.umc.yeogi_gal_lae.api.user.repository;
 
 import com.umc.yeogi_gal_lae.api.user.domain.User;
+
+import java.util.List;
 import java.util.Optional;
+
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    // 투표 이력에 상관 없이 현재 여행 계획에 해당하는 user 가져오기
+    @Query("SELECT u FROM User u WHERE u.vote IS NULL or u.vote.tripPlan.id = :tripId")
+    List<User> findUsersByVoteTripPlanId(@Param("tripId") Long tripId);
 }
