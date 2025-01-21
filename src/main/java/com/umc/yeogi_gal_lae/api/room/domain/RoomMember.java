7b0f53dee1c674308cde9d9efbca975bfe574b01
MINSOO CHOI <4987kk@naver.com>
2025-01-21 18:11:26 +0900

    [refactor]: RoomMember 엔티티 수정 및 연관관계 설정 개선
                   - User와의 연관관계 추가:
                     - RoomMember 엔티티에 User와의 ManyToOne 매핑 추가.
                     - EmbeddedId의 userId와 매핑하도록 MapsId 적용.
                   - FetchType.LAZY 추가:
                     - Room과 User의 연관관계에 대해 지연 로딩 적용으로 성능 최적화.
                   - 중복 매핑 방지:
                     - @JoinColumn에 insertable=false, updatable=false를 설정하여 중복 매핑 방지.
                   - 코드 정리:
                     - 불필요한 주석 제거 및 필드 간 가독성 개선. #4

--- src/main/java/com/umc/yeogi_gal_lae/api/room/domain/RoomMember.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/room/domain/RoomMember.java
@@ -1,5 +1,6 @@
 package com.umc.yeogi_gal_lae.api.room.domain;
 
+import com.umc.yeogi_gal_lae.api.user.domain.User;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -12,10 +13,14 @@ public class RoomMember {
     private RoomMemberId id;
 
     @MapsId("roomId") // EmbeddedId의 roomId와 매핑
-    @ManyToOne
-    @JoinColumn(name = "room_id") // room_id로 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", insertable = false, updatable = false) // 중복 매핑 방지
     private Room room;
 
+    @MapsId("userId") // RoomMemberId의 userId와 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false) // 중복 매핑 방지
+    private User user;
 //    @MapsId("userId") // EmbeddedId의 userId와 매핑
 //    @ManyToOne
 //    @JoinColumn(name = "user_id") // user_id로 매핑
