90e3249719c6b75e97a11128e632752e8c992826
margie1a <4987kk@naver.com>
2025-02-06 00:14:01 +0900

    [feat]: controller 주소 api 추가

--- src/main/java/com/umc/yeogi_gal_lae/api/notification/controller/NotificationController.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/notification/controller/NotificationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/notifications")
+@RequestMapping("/api/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
 
