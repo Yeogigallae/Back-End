919cd7086fb35de55f948c6e39f6d514dd047001
parkmineum <a01087164428@gmail.com>
2025-02-06 16:10:13 +0900

    [fix] 서버 정적 리소스 문제 오류로 인해 헬스 체킹 수정

--- src/main/java/com/umc/yeogi_gal_lae/CheckServerStatusController.java
+++ src/main/java/com/umc/yeogi_gal_lae/CheckServerStatusController.java
@@ -1,5 +1,7 @@
 package com.umc.yeogi_gal_lae;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,10 @@ import java.time.LocalDateTime;
 
 @RestController
 public class CheckServerStatusController {
-    @GetMapping("/health")
-    public String healthCheck() {
-        return LocalDateTime.now().toString();
+
+    @Operation(description = "Docker 컨테이너 가동 시 헬스 체킹을 위한 컨트롤러")
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Server is running");
     }
 }
