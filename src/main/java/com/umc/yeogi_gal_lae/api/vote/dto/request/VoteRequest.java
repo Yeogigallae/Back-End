ee9de8945f9e7ee649a73b2c718e426f05cbebd3
parkmineum <a01087164428@gmail.com>
2025-02-07 19:13:19 +0900

    [fix] 안쓰는 코드 제거

--- src/main/java/com/umc/yeogi_gal_lae/api/vote/dto/request/VoteRequest.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/vote/dto/request/VoteRequest.java
@@ -8,17 +8,7 @@ import net.minidev.json.annotate.JsonIgnore;
 public class VoteRequest {
 
 
-    @Getter @Setter
-    public static class createVoteRoomReq{
-
-        @NotNull(message = "특정 여행의 투표방이 되기 위함")
-        private Long tripId;
-
-        // masterId
-
-    }
-
-    @Getter @Setter
+    @Getter
     public static class createVoteReq{
 
         @JsonIgnore      // 클라이언트에서 받지 않음.
