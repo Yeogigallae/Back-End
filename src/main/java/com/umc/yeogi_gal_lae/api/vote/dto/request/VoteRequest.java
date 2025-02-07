4a38cb62ae4b5e78a5dc833e05b1915a2a60c2ae
parkmineum <a01087164428@gmail.com>
2025-02-07 19:14:19 +0900

    [refactor] JsonIgnore 관련 설정 추가

--- src/main/java/com/umc/yeogi_gal_lae/api/vote/dto/request/VoteRequest.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/vote/dto/request/VoteRequest.java
@@ -1,5 +1,6 @@
 package com.umc.yeogi_gal_lae.api.vote.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import net.minidev.json.annotate.JsonIgnore;
@@ -9,9 +10,11 @@ public class VoteRequest {
 
 
     @Getter
+    @Setter
     public static class createVoteReq{
 
         @JsonIgnore      // 클라이언트에서 받지 않음.
+        @Schema(hidden = true)
         private String userEmail;
 
         @NotNull(message = "투표에 해당하는 여행 계획 Id")
