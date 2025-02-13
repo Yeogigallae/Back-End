b51611b34f5dc1c487b90fa93db82a16653922e0
Gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-02-14 04:53:33 +0900

    [feat] place response 수정

--- src/main/java/com/umc/yeogi_gal_lae/api/place/dto/response/PlaceResponse.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/place/dto/response/PlaceResponse.java
@@ -3,19 +3,20 @@ package com.umc.yeogi_gal_lae.api.place.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
 @Builder
 @AllArgsConstructor
-@ToString
 public class PlaceResponse {
 
-    private Long placeId;
-    private Long roomId;
+    private Long id;
     private String placeName;
     private String address;
-    private Double lat;
-    private Double lng;
+    private Double latitude;
+    private Double longitude;
+    private String image;
+    private String description;
+    private String userName;
+    private String profileImage;
 
 }
