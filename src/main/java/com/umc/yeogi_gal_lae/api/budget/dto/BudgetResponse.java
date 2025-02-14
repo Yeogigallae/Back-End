2a9f241d9e3886c78d73fb49453ef4bb21b7d0d9
Gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-02-14 20:39:04 +0900

    [feat] budget dto 설정

--- src/main/java/com/umc/yeogi_gal_lae/api/budget/dto/BudgetResponse.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/budget/dto/BudgetResponse.java
@@ -4,15 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class BudgetResponse {
-    private String day;
-    private double mealBudget;
-    private double activityBudget;
-    private double shoppingBudget;
-    private double transportBudget;
+    private Long id;           // Budget 엔티티 기본키
+    private Long aiCourseId;   // 연결된 AICourse의 id
+    private Long tripPlanId;   // AICourse에 연결된 TripPlan의 id
+    private Long roomId;       // TripPlan에 연결된 Room의 id
 }
