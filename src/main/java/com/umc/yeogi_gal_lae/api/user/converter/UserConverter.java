8c20dbdfadc51e4e2d51549684eb98b98c948e9a
GithubKangMin <158579562+GithubKangMin@users.noreply.github.com>
2025-02-16 00:13:16 +0900

    마이페이지

--- src/main/java/com/umc/yeogi_gal_lae/api/user/converter/UserConverter.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/user/converter/UserConverter.java
@@ -1,14 +1,15 @@
 package com.umc.yeogi_gal_lae.api.user.converter;
 
+import com.nimbusds.openid.connect.sdk.UserInfoResponse;
 import com.umc.yeogi_gal_lae.api.user.domain.User;
 import com.umc.yeogi_gal_lae.api.user.dto.response.UserResponseDTO;
 
 public class UserConverter {
     public static UserResponseDTO.JoinResultDTO toJoinResultDTO(User user) {
         return new UserResponseDTO.JoinResultDTO(
-            user.getEmail(),
-            user.getUsername(),
-            user.getProfileImage()
+                user.getEmail(),
+                user.getUsername(),
+                user.getProfileImage()
         );
     }
 }
