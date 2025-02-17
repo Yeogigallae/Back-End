018be01e5554555a46499d329ffcf8734ed12f98
Gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-02-17 11:26:23 +0900

    [refactor] place 추가 시 tripPlan 기준으로 추가

--- src/main/java/com/umc/yeogi_gal_lae/api/place/repository/PlaceRepository.java
+++ src/main/java/com/umc/yeogi_gal_lae/api/place/repository/PlaceRepository.java
@@ -1,12 +1,12 @@
 package com.umc.yeogi_gal_lae.api.place.repository;
 
 import com.umc.yeogi_gal_lae.api.place.domain.Place;
-import com.umc.yeogi_gal_lae.api.room.domain.Room;
+import com.umc.yeogi_gal_lae.api.tripPlan.domain.TripPlan;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
-    List<Place> findAllByRoomId(Long roomId);
+    List<Place> findAllByTripPlanId(Long tripPlanId);
 
-    List<Place> findByRoom(Room room);
+    List<Place> findByTripPlan(TripPlan tripPlan);
 }
